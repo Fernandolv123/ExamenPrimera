340cba8adc3e5a5c5711bb3049bcf2c2e20760ff
FernandoLV <ferlorenzovazquez@gmail.com>
2023-03-03 17:44:45 +0100

    testSolucion1Incorrecta terminado y comprobado

--- matematicas/src/test/java/es/rlopezga/mates/matematicas/tests/CalculadoraTest.java
+++ matematicas/src/test/java/es/rlopezga/mates/matematicas/tests/CalculadoraTest.java
@@ -29,4 +29,10 @@ public class CalculadoraTest {
 		//solucion -2
 		Assertions.assertEquals(c.solucion2(-1, -7, -10),-2);
 	}
+	
+	@Test
+	void testSolucion1Incorrecta() {
+		//Lanza una excepcion de tipo ArithmeticException
+		Assertions.assertThrows(ArithmeticException.class, () -> {c.solucion1(1, 1, 1);});
+	}
 }
